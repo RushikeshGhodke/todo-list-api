5115ed2afe9397c3d22d6822fae0eb4d2dcfb0c6
RushikeshGhodke <rushikeshghodke7455@gmail.com>
2025-10-08 21:43:47 +0530

    user register api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,10 +4,24 @@ import ApiError from "../utils/ApiError.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
-    console.log(name);
 
-    return res
-        .status(200)
-        .json(new ApiResponse(200, name, "User registered successfully"));
+    try {
+        if (name && email && password) {
+            console.log(name, email, password);
+            return res
+                .status(200)
+                .json(new ApiResponse(200, { name, email, password }, "User data received successfully"));
+        } else {
+            return res
+                .status(400)
+                .json(new ApiError(400, "Validation Error", ["Name, Email, Password shouldn't be empty."]));
+        }
+    } catch (error) {
+        return res
+            .status(500)
+            .json(new ApiError(500, "Server Error", ["Something went wrong while registering user."]));
+    }
+
+
 
 });
